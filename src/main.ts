e27d07b22d47f8c9bf99a745649b77363d5bd205
rahul shakya <rahulsaqya@gmail.com>
2023-04-17 17:23:50 +0545

    added port

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as dotenv from 'dotenv';
 async function bootstrap() {
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
   const options = new DocumentBuilder()
     .setTitle('OpenAPI Documentation')
@@ -12,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
